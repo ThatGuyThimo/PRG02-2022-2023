8850b19a253f09cfdb957d097ee0cc31f85dc98d
antwanvdm <antwanvdm@gmail.com>
2022-12-15 11:37:42 +0100

    one-to-many week 5 finished

---
+++ week5/examples/one-to-many-finished/includes/database.php
@@ -0,0 +1,9 @@
+<?php
+
+// General settings
+$host = "localhost";
+$database = "my_webshop";
+$user = "root";
+$password = "";
+
+$db = mysqli_connect($host, $user, $password, $database) or die("Error: " . mysqli_connect_error());
